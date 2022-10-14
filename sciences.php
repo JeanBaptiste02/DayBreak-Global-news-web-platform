ec608f2ac260fb9288b0552102043b07a74da169
sriguru <sriguruelumalai@gmail.com>
2022-10-14 11:35:07 +0200

    ajout des categories et mise a jour de index

---
+++ sciences.php
@@ -0,0 +1,20 @@
+<?php
+$titre = "DailyBreaks";
+$link = "./css/style.css";
+require "./include/header.inc.php";
+?>
+
+    <main>  
+        <section>
+           <p>test</p>
+        </section>
+    </main>
+
+        <!-- lien du fichier js -->
+        <script src="js/script.js">
+
+        </script>
+        
+<?php
+    require "./include/footer.inc.php";
+?>
